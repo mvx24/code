ff14c70d516424d12c27e02a5529776773d3dc94
marc <marc@symbioticsoftware.com>
2019-05-30 16:15:35 -0700

    Adds basic mjml emails

---
+++ fastapi/emails/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const mjml2html = require('mjml');
+
+const sourceDir = path.resolve('./src');
+const buildDir = path.resolve('./build');
+
+fs.readdir(sourceDir, (err, files) => {
+  files.forEach(file => {
+    if (file[0] === '_') return;
+    const filePath = path.join(sourceDir, file);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      const output = mjml2html(data, { filePath });
+      const outputFile = path.join(buildDir, `${path.basename(file, '.mjml')}.html`);
+      fs.writeFile(outputFile, output.html, () => {});
+    });
+  });
+});

f4ed1fe1a92d551505d01089c0e5e30c4eb40042
marc <marc@symbioticsoftware.com>
2019-08-30 16:29:18 -0700

    Moved code to use electron preload files

---
+++ electron/preload.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+  if (process.env.NODE_ENV === 'development') {
+    // Setup babel require hooks to translate es6 importing/exporting and jsx
+    require('@babel/register');
+    // Add a CSS require hook to append CSS files into the app
+    var fs = require('fs');
+    var loadedCSS = {};
+    require.extensions['.css'] = function(module, filename) {
+      if (!loadedCSS[filename]) {
+        loadedCSS[filename] = true;
+        var css = fs.readFileSync(filename, 'utf8').trim();
+        if (css.length) {
+          var style = document.createElement('style');
+          style.type = 'text/css';
+          style.textContent = css;
+          document.head.appendChild(style);
+        }
+      }
+    };
+    // Start the main app
+    require('./src');
+  } else {
+    // Start the app using bundled JS and CSS
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'bundle.css';
+    document.head.appendChild(link);
+    require('./bundle');
+  }
+});

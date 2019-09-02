77077d7b6f64a58ff83b742680a0340f7b51e4bf
marc <marc@symbioticsoftware.com>
2019-09-01 23:11:35 -0700

    Cookie functions

---
+++ browser/cookies.js
@@ -0,0 +1,25 @@
+export function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === `${name}=`) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+export function setCookie(name, value, expires, path) {
+  document.cookie = [
+    `${name}=${encodeURIComponent(value)}`,
+    expires ? expires.toUTCString() : null,
+    path,
+    'Secure',
+  ]
+    .filter(Boolean)
+    .join('; ');
+}

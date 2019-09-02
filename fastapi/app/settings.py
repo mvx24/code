0fdcf96d5c8227b68199b377146675df2ae7badf
marc <marc@symbioticsoftware.com>
2019-09-01 19:59:55 -0700

    Formatting

--- fastapi/app/settings.py
+++ fastapi/app/settings.py
@@ -19,14 +19,14 @@ class Settings(BaseSettings):
     MAX_PAGE_SIZE: int = 250
 
     # Access Tokens
-    TOKEN_ISSUER: str = 'pyjwt'
-    TOKEN_ALGORITHM: str = 'HS256'
-    TOKEN_EXPIRATION: int = (2*7*24*60*60)
+    TOKEN_ISSUER: str = "pyjwt"
+    TOKEN_ALGORITHM: str = "HS256"
+    TOKEN_EXPIRATION: int = (2 * 7 * 24 * 60 * 60)
 
     # CORS Configuration
     CORS_ALLOW_ORIGINS: List[str] = []
-    CORS_ALLOW_ORIGIN_REGEX: str = ''
-    CORS_ALLOW_METHODS: List[str] = ['GET']
+    CORS_ALLOW_ORIGIN_REGEX: str = ""
+    CORS_ALLOW_METHODS: List[str] = ["GET"]
     CORS_ALLOW_HEADERS: List[str] = []
     CORS_ALLOW_HEADERS: List[str] = []
     CORS_ALLOW_CREDENTIALS: bool = False
@@ -34,12 +34,12 @@ class Settings(BaseSettings):
     CORS_MAX_AGE: int = 60
 
     # Database
-    DB_DRIVERNAME: str = 'postgresql'
+    DB_DRIVERNAME: str = "postgresql"
     DB_USERNAME: str
     DB_PASSWORD: str
-    DB_HOST: str = 'localhost'
+    DB_HOST: str = "localhost"
     DB_PORT: int = 5432
-    DB_DATABASE: str = 'postgres'
+    DB_DATABASE: str = "postgres"
     DB_SSL: bool = False
     DB_POOL_SIZE: int = 10
     DB_POOL_OVERFLOW: int = 5
@@ -47,15 +47,15 @@ class Settings(BaseSettings):
     DB_POOL_RECYCLE_QUERIES: int = 50000
 
     # Redis
-    REDIS_HOST: str = 'localhost'
+    REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_DATABASE: int = 0
     REDIS_PASSWORD: str = None
 
     # Email
     EMAIL_FROM_EMAIL: EmailStr = None
-    EMAIL_FROM_NAME: str = ''
-    EMAIL_SUBJECT_PREFIX: str = ''
+    EMAIL_FROM_NAME: str = ""
+    EMAIL_SUBJECT_PREFIX: str = ""
     SMTP_HOST: str = None
     SMTP_USER: str = None
     SMTP_PASSWORD: str = None
@@ -75,10 +75,10 @@ class Settings(BaseSettings):
 
 
 # Load up environment variables and initialize the settings
-env = os.getenv('ASGI_ENV', Env.DEVELOPMENT)
-load_dotenv(dotenv_path=os.path.abspath(f'.env.{env}.local'))
-load_dotenv(dotenv_path=os.path.abspath(f'.env.{env}'))
-load_dotenv(dotenv_path=os.path.abspath('.env.local'))
+env = os.getenv("ASGI_ENV", Env.DEVELOPMENT)
+load_dotenv(dotenv_path=os.path.abspath(f".env.{env}.local"))
+load_dotenv(dotenv_path=os.path.abspath(f".env.{env}"))
+load_dotenv(dotenv_path=os.path.abspath(".env.local"))
 load_dotenv()
 
 settings = Settings(ENV=env)

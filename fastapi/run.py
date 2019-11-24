58e14e945e55a2ce829d380314749ec6607f2c62
marc <marc@symbioticsoftware.com>
2019-11-23 23:14:11 -0800

    Update run file

--- fastapi/run.py
+++ fastapi/run.py
@@ -1,6 +1,24 @@
 #!./venv/bin/python
+import os
 import uvicorn
 
 
-if __name__ == '__main__':
-    uvicorn.run('app.asgi:app', host='0.0.0.0', port=8000, reload=True)
+if __name__ == "__main__":
+    port = 8000
+    if os.getenv("FASTAPI_DOCKER"):
+        os.system("./venv/bin/python wait.py")
+        os.system("./venv/bin/alembic upgrade head")
+        os.system("./venv/bin/python fixtures.py")
+        if os.getenv("ASGI_ENV") == "production":
+            port = 443
+        uvicorn.run(
+            "app.asgi:app",
+            host="0.0.0.0",
+            port=port,
+            ssl_keyfile="./certs/domain.key",
+            ssl_certfile="./certs/domain.crt",
+        )
+    else:
+        os.system("./venv/bin/alembic upgrade head")
+        os.system("./venv/bin/python fixtures.py")
+        uvicorn.run("app.asgi:app", host="0.0.0.0", port=port, reload=True)

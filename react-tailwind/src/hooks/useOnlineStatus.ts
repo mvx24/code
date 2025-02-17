1457c401e896f825a1a38066fe464b7a9463eeaa
Marc <marcangelone@gmail.com>
2025-02-16 22:55:31 -0800

    useOnlineStatus

---
+++ react-tailwind/src/hooks/useOnlineStatus.ts
@@ -0,0 +1,22 @@
+// Example from:
+// https://react.dev/reference/react/useSyncExternalStore#extracting-the-logic-to-a-custom-hook
+
+import { useSyncExternalStore } from 'react';
+
+export function useOnlineStatus() {
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot);
+  return isOnline;
+}
+
+function getSnapshot() {
+  return navigator.onLine;
+}
+
+function subscribe(callback: () => void) {
+  window.addEventListener('online', callback);
+  window.addEventListener('offline', callback);
+  return () => {
+    window.removeEventListener('online', callback);
+    window.removeEventListener('offline', callback);
+  };
+}

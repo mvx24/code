2c2ad842758582dcae5f6e93aad76e79c77f2d5b
Marc <marcangelone@gmail.com>
2025-02-22 01:51:20 -0800

    Icon

--- react-tailwind/src/components/Icon.tsx
+++ react-tailwind/src/components/Icon.tsx
@@ -13,7 +13,7 @@ interface _IconProps extends IconProps {
   [key: string]: unknown;
 }
 
-export function Icon({ title, children, solid, className, ...rest }: _IconProps) {
+export function Icon({ title, children, solid, className, viewBox, ...rest }: _IconProps) {
   const titleId = `icon-${Math.floor((1 + Math.random()) * 0x10000)}`;
   const hasSize = className && (className.startsWith('size-') || className.includes(' size-'));
   const hasWidth = className && (className.startsWith('w-') || className.includes(' w-'));
@@ -22,6 +22,7 @@ export function Icon({ title, children, solid, className, ...rest }: _IconProps)
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
       aria-labelledby={title && titleId}
       className={clsx(
         'inline-block align-text-bottom',
